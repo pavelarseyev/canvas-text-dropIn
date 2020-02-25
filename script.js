da9dbb659492705dedc66648487ac4b402097b46
pavel_arseyev <pavel.arseev@bigdropinc.com>
2020-02-25 16:15:00 +0200

    some cool thing

---
+++ script.js
@@ -0,0 +1,86 @@
+let textContainer = document.querySelectorAll(".canvas-text");
+
+//resize
+window.addEventListener("resize", function() {
+    createCanvasText(textContainer);
+});
+
+//load
+window.addEventListener("load", function() {
+    createCanvasText(textContainer);
+});
+
+function createCanvasText(items) {
+    [...items].forEach( (item, i) => {
+        let top = item.getBoundingClientRect().top;   
+
+        if(top < window.innerHeight && top > 0 - item.offsetHeight) {    
+            Array.from(item.querySelectorAll(".canvas-wrapper")).forEach(i => i.remove());
+
+            let maxWidth = item.offsetWidth;
+            let text = item.textContent.trim();
+            let itemStyles = window.getComputedStyle(item);
+            let fontsize = itemStyles.fontSize;
+            let fontfamily = itemStyles.fontFamily;
+            let fontweight = itemStyles.fontWeight;
+            let lineheight = itemStyles.lineHeight;
+            let color = itemStyles.color;
+        
+            wrapText(item, text, maxWidth, fontfamily, fontsize, lineheight, fontweight, color);
+
+            if(!item.classList.contains("loaded")) {
+                setTimeout(function() {
+                    item.classList.add("loaded");
+                }, 0);
+            }
+        }
+    });
+}
+
+function wrapText( element, text, maxWidth, fontFamily, fontSize, lineHeight, fontWeight, color) {
+    let canvasWrapper = document.createElement("div");
+    canvasWrapper.classList.add("canvas-wrapper");
+
+
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
+
+    let spacingWidth = ctx.measureText(" ").width + 2;
+
+    let counter = 0;
+    let yForCanvas = 0;
+    let yForText = parseInt(fontSize);
+    
+    let words = text.replace(/(\s{2,})+/g, " ").split(' ');    
+    let line = '';
+
+    for (let n = 0; n < words.length; n++) {
+        let testLine = line + words[n] + " ";
+        let testWidth = ctx.measureText(testLine).width;
+        
+        if (testWidth >= maxWidth+spacingWidth && n > 0) {
+            canvasWrapper = document.createElement("div");
+            canvasWrapper.classList.add("canvas-wrapper");
+
+            canvas = document.createElement("canvas");
+            ctx = canvas.getContext("2d");
+            line = words[n] + " ";
+            yForCanvas += parseInt(lineHeight);
+            counter++;
+        } else {
+            line = testLine;
+            canvas.height = parseInt(lineHeight);
+            canvas.width = maxWidth;
+            ctx.font = fontWeight + " " + fontSize + " " + fontFamily;
+            
+            // ctx.fillStyle = color;
+            ctx.fillStyle = "black";
+            canvasWrapper.style.top = `${yForCanvas}px`;
+            canvas.style.cssText = `transition-delay: ${counter/10 * 3}s`;
+            
+            ctx.fillText(line, 0, yForText);
+            canvasWrapper.appendChild(canvas);
+            element.appendChild(canvasWrapper);
+        }
+    }
+}
